59a6ca1742e0a2d1ae4bd857885579d187dfdccd
Siddharth Jain <jain.siddharth123@gmail.com>
2015-03-22 07:21:23 +0530

    Location received, query to be generated

--- app/src/main/java/com/iiitd/team/test/Main.java
+++ app/src/main/java/com/iiitd/team/test/Main.java
@@ -1,6 +1,12 @@
 package com.iiitd.team.test;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
@@ -12,8 +18,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class Main extends ActionBarActivity implements ActionBar.TabListener {
@@ -26,7 +34,67 @@ public class Main extends ActionBarActivity implements ActionBar.TabListener {
     private String[] tabs = { "Post Request", "Pending Request"};
     private ListView mListView;
     private ArrayAdapter<CharSequence> navAdapter;
+    public LocationManager locationManager;
+    public Location myCurrentLocation=null;
+    Spinner fertiliserList;
 
+
+
+    public class CurrentLocation implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                myCurrentLocation = location;
+                /*Toast mToast = Toast.makeText(getApplicationContext(), "location changed", Toast.LENGTH_SHORT);
+                mToast.show();*/
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+    public void postQuery(View v){
+
+        Toast mToast = Toast.makeText(this,"entered postQuery", Toast.LENGTH_SHORT);
+        mToast.show();
+         //postrequest = (Button)findViewById(R.id.buttonRequestButton);
+        /*postrequest.setOnClickListener(new View.OnClickListener() {
+            @Override*/
+           // public void onClick(View v) {
+                    //locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        CurrentLocation mCurrentLocation = new CurrentLocation();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mCurrentLocation);
+
+                    myCurrentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    //mylistener = new MyLocationListener();
+                    if(myCurrentLocation == null){
+                        // leads to the settings because there is no last known location
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                        mToast = Toast.makeText(this,"Location null", Toast.LENGTH_SHORT);
+                        mToast.show();
+                    }
+
+                        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new CurrentLocation() );
+                    //while(myCurrentLocation==null) {
+                       /* mToast = Toast.makeText(this,""+myCurrentLocation.getLatitude(), Toast.LENGTH_SHORT);
+                        mToast.show();
+                    //}*/
+
+    }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +103,11 @@ public class Main extends ActionBarActivity implements ActionBar.TabListener {
         actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         //actionBar.setDisplayHomeAsUpEnabled(true);
+        fertiliserList = (Spinner)findViewById(R.id.fertiliserMenu);
         mAdapter = new TabAdaper(getSupportFragmentManager());
         viewPager.setAdapter(mAdapter);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
         for (String tab_name : tabs) {
             actionBar.addTab(actionBar.newTab().setText(tab_name)
                     .setTabListener(this));
@@ -76,7 +146,7 @@ public class Main extends ActionBarActivity implements ActionBar.TabListener {
 
         }
 
-        Spinner fertiliserList = (Spinner)findViewById(R.id.fertiliserMenu);
+
 
 
         final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ferti,android.R.layout.simple_spinner_dropdown_item);
@@ -132,16 +202,10 @@ public class Main extends ActionBarActivity implements ActionBar.TabListener {
         // show respected fragment view
         viewPager.setCurrentItem(tab.getPosition());
 
-        /*if(tab.getText().equals("Post Request")) {
 
-            PostRequestFragment mPostRequestFragment = new PostRequestFragment();
-                    *//*Bundle args = new Bundle();
-                    args.putInt(mPendingRequestFragment.ARG_POSITION, position);
-                     mPendingRequestFragment.setArguments(args);*//*
-            fragmentTransaction.replace(R.id.drawer_layout, mPostRequestFragment);
-            fragmentTransaction.addToBackStack(null);
+
         }
-        else{
+        /*else{
                 PendingRequestFragment mPendingRequestFragment = new PendingRequestFragment();
                 fragmentTransaction.replace(R.id.drawer_layout, mPendingRequestFragment);
                 fragmentTransaction.addToBackStack(null);
@@ -151,7 +215,7 @@ public class Main extends ActionBarActivity implements ActionBar.TabListener {
 
 // Commit the transaction
 
-    }
+
 
 
     @Override
--- app/src/main/java/com/iiitd/team/test/PostRequestFragment.java
+++ app/src/main/java/com/iiitd/team/test/PostRequestFragment.java
@@ -1,5 +1,8 @@
 package com.iiitd.team.test;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 /**
  * Created by hp on 21-03-2015.
@@ -19,6 +23,7 @@ public class PostRequestFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.post_request, container, false);
+        Button postrequest= (Button)rootView.findViewById(R.id.buttonRequestButton);
 
         return rootView;
     }
